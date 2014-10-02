e5b2888cf4bce7efc66529fe1b3c0866b42cf687
Tay Yang Shun <tay.yang.shun@gmail.com>
2014-10-02 18:13:51 +0800

    [A3] Projection models done

--- assgs/assg3/assg3.py
+++ assgs/assg3/assg3.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 from math import *
 
 def deg_to_rad(deg):
@@ -47,23 +48,32 @@ def quatrot(p, q):
 def quat2rot(q):
   # Returns a 3x3 rotation matrix parameterized with
   # the elements of an input quaternion
-  q0, q1, q2, q3 = q
-  return np.matrix([[q0**2 + q1**2 - q2**2 - q3**2, 2*(q1*q2 - q0*q3), 2*(q1*q3 + q0*q2)],
-                    [2*(q1*q2 + q0*q3), q0**2 + q2**2 - q1**2 - q3**2, 2*(q2*q3 - q0*q1)],
-                    [2*(q1*q3 - q0*q2), 2*(q2*q3 + q0*q1), q0**2 + q3**2 - q1**2 - q2**2]])
+  q_0, q_1, q_2, q_3 = q
+  return np.matrix([[q_0**2 + q_1**2 - q_2**2 - q_3**2, 2*(q_1*q_2 - q_0*q_3), 2*(q_1*q_3 + q_0*q_2)],
+                    [2*(q_1*q_2 + q_0*q_3), q_0**2 + q_2**2 - q_1**2 - q_3**2, 2*(q_2*q_3 - q_0*q_1)],
+                    [2*(q_1*q_3 - q_0*q_2), 2*(q_2*q_3 + q_0*q_1), q_0**2 + q_3**2 - q_1**2 - q_2**2]])
 
 pts = np.zeros([11, 3]) 
-pts[0, :] = [-1, -1, -1]
-pts[1, :] = [1, -1, -1]
-pts[2, :] = [1, 1, -1]
-pts[3, :] = [-1, 1, -1]
-pts[4, :] = [-1, -1, 1]
-pts[5, :] = [1, -1, 1]
-pts[6, :] = [1, 1, 1]
-pts[7, :] = [-1, 1, 1]
-pts[8, :] = [-0.5, -0.5, -1] 
-pts[9, :] = [0.5, -0.5, -1] 
-pts[10, :] = [0, 0.5, -1]
+pts[0,:] = [-1, -1, -1]
+pts[1,:] = [1, -1, -1]
+pts[2,:] = [1, 1, -1]
+pts[3,:] = [-1, 1, -1]
+pts[4,:] = [-1, -1, 1]
+pts[5,:] = [1, -1, 1]
+pts[6,:] = [1, 1, 1]
+pts[7,:] = [-1, 1, 1]
+pts[8,:] = [-0.5, -0.5, -1] 
+pts[9,:] = [0.5, -0.5, -1] 
+pts[10,:] = [0, 0.5, -1]
+
+# Constants
+u_0 = 0
+v_0 = 0
+B_u = 1
+B_v = 1
+k_u = 1
+k_v = 1
+f = 1
 
 # Calculating camera positions for each frame
 initial_pos = [0, 0, 0, -5]
@@ -88,6 +98,48 @@ for i in range(3):
   orntn_new = camera_rot_mat * orntn_new
   camera_orntns.append(orntn_new)
 
-camera_orntns = [approx_mat(m) for m in camera_orntns]
+camera_orntns = [np.array(approx_mat(m)) for m in camera_orntns]
 # Camera orientations for each frame
 quatmat_1, quatmat_2, quatmat_3, quatmat_4 = camera_orntns
+
+def perspective_proj(s_p, t_f, i_f, j_f, k_f):
+  sptf = s_p - t_f
+  u_fp = f * float(np.dot(sptf, i_f)) / np.dot(sptf, k_f) * B_u + u_0
+  v_fp = f * float(np.dot(sptf, j_f)) / np.dot(sptf, k_f) * B_v + v_0
+  return [approx(p) for p in (u_fp, v_fp)]
+
+def orthographic_proj(s_p, t_f, i_f, j_f, k_f):
+  sptf = s_p - t_f
+  u_fp = float(np.dot(sptf, i_f)) * B_u + u_0
+  v_fp = float(np.dot(sptf, j_f)) * B_v + v_0
+  return [approx(p) for p in (u_fp, v_fp)]
+
+PLOT_HORIZONTAL_COUNT = 2
+PLOT_VERTICAL_COUNT = 2
+PLOT_PADDING = 0.5
+PLOT_MARGIN = 0.2
+PLOT_FONT_SIZE = 18
+NUMBER_OF_POINTS_CUBE = 8
+
+def generate_projection_plots(pts, proj_fn, proj_name):
+  fig = plt.figure()
+  plt.subplots_adjust(hspace=PLOT_PADDING, wspace=PLOT_PADDING)
+  for i in range(4):
+    projected_pts = [proj_fn(pt, np.array(camera_pos[i][1:]), camera_orntns[i][0], \
+                       camera_orntns[i][1], camera_orntns[i][2]) for pt in pts]
+    plt.subplot(PLOT_HORIZONTAL_COUNT, PLOT_VERTICAL_COUNT, i+1)
+    plt.margins(PLOT_MARGIN)
+    plt.title('Frame ' + str(i+1))
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.axis('equal')
+    for index, pt in list(enumerate(projected_pts)):
+      # Use red for triangle points for easy identification
+      plt.plot(pt[0], pt[1], 'bo' if index < NUMBER_OF_POINTS_CUBE else 'ro')
+  
+  plt.suptitle(proj_name, fontsize=PLOT_FONT_SIZE)
+  plt.show()
+  fig.savefig(proj_name + '.png')
+
+generate_projection_plots(pts, perspective_proj, 'Perspective Projection')
+generate_projection_plots(pts, orthographic_proj, 'Orthographic Projection')
