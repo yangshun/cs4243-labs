749a986ca25ca8cf1d1e18e82650dfe22c3af8e8
Tay Yang Shun <tay.yang.shun@gmail.com>
2014-10-02 13:08:18 +0800

    [A3] Quaternion rotation done

--- assgs/assg3/assg3.py
+++ assgs/assg3/assg3.py
@@ -1,4 +1,5 @@
 import numpy as np
+from math import *
 
 pts = np.zeros([11, 3]) 
 pts[0, :] = [-1, -1, -1]
@@ -13,13 +14,23 @@ pts[8, :] = [-0.5, -0.5, -1]
 pts[9, :] = [0.5, -0.5, -1] 
 pts[10, :] = [0, 0.5, -1]
 
-def quatmult(q, p):
-  s_q, v_q = q[0], np.array(q[1:])
+def degToRad(deg):
+  return float(deg)/180 * pi
+
+def conjugate(quat):
+  return quat[0:1] + np.negative(quat[1:]).tolist()
+
+def approx(value):
+  val = round(value, 6)
+  return 0.0 if val == 0.0 else val
+
+def quatmult(p, q):
   s_p, v_p = p[0], np.array(p[1:])
-  s_qp = s_q * s_p - np.dot(v_q, v_p)
-  v_qp = np.cross(v_q, v_p) + s_q * v_p + s_p * v_q
-  out = [s_qp]
-  out.extend(v_qp)
+  s_q, v_q = q[0], np.array(q[1:])
+  s_pq = s_q * s_p - np.dot(v_q, v_p)
+  v_pq = np.cross(v_p, v_q) + s_q * v_p + s_p * v_q
+  out = [s_pq]
+  out.extend(v_pq)
   return out
 
 def quatmult2(p, q):
@@ -30,3 +41,15 @@ def quatmult2(p, q):
   out[2] = p[0]*q[2] - p[1]*q[3] + p[2]*q[0] + p[3]*q[1]
   out[3] = p[0]*q[3] + p[1]*q[2] - p[2]*q[1] + p[3]*q[0]
   return out
+
+def quatrot(p, q): 
+  return [approx(x) for x in quatmult(quatmult(q, p), conjugate(q))]
+
+point = [0, 0, 0, -5]
+rot = [cos(degToRad(-15)), 0, sin(degToRad(-15)), 0]
+print point
+for i in range(3):
+  point = quatrot(point, rot)
+  print point
+
+
